4fc0885780ffbbcc67aef3b664631be6e995aa4e
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2017-12-30 21:59:49 -0800

    more tests while cpu is spinning

--- test9.py
+++ test9.py
@@ -1,9 +1,12 @@
 import wpilib
 import time
 import numpy
+import csv
 
 
-# I get mean = 40 ms, std dev ~ 300 microseconds
+# at period 40 ms, I get mean = 40 ms, std dev ~ 300 microseconds
+# at period 5 ms, I get mean = 5 ms, std dev ~ 280 microseconds
+# at period 1 ms, I get mean = 1 ms, std dev ~ 800 microseconds
 
 start_times = []
 
@@ -13,7 +16,7 @@ def runnable():
     start_times.append(start)
 
 notifier = wpilib.Notifier(runnable)
-notifier.startPeriodic(0.04)
+notifier.startPeriodic(0.001)
 
 
 timer = wpilib.Timer()
@@ -27,6 +30,11 @@ wpilib.Timer.delay(2)
 
 times = numpy.array(start_times)
 time_diffs = times[1:] - times[:-1]
+
+with open('timing.csv', 'w') as f:
+    writer = csv.writer(f)
+    for time in start_times:
+        writer.writerow([time])
 
 print (time_diffs.mean())
 print (time_diffs.std())
